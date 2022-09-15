39a107844110edf5988b681198f205c1f7a51990
Dawid Sula <dsl@touk.pl>
2022-09-15 10:57:25 +0200

    Moved test for ingestion time

--- flink111/model/src/test/scala/pl/touk/nussknacker/compatibility/TimestampTest.scala
+++ flink111/model/src/test/scala/pl/touk/nussknacker/compatibility/TimestampTest.scala
@@ -3,5 +3,17 @@ package pl.touk.nussknacker.compatibility
 import org.scalatest.matchers.should.Matchers
 import pl.touk.nussknacker.compatibility.common.BaseTimestampTest
 import pl.touk.nussknacker.compatibility.flink111.Flink111Spec
+import pl.touk.nussknacker.engine.process.helpers.SampleNodes.SinkForLongs
 
-class TimestampTest extends BaseTimestampTest with Flink111Spec with Matchers
+class TimestampTest extends BaseTimestampTest with Flink111Spec with Matchers {
+
+  // This test depends on default time characteristics in Flink 1.11 -
+  // - ingestion time. This default was changed to event time in 1.12.
+  test("should handle ingestion time") {
+    runWithAssigner(None)
+
+    //5 seconds should be enough to run this test...-
+    SinkForLongs.data.head.toDouble shouldBe (System.currentTimeMillis().toDouble +- 5000)
+  }
+
+}
