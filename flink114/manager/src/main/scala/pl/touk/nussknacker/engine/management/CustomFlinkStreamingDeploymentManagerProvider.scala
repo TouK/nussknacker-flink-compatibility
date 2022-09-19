0553288ae9b223a5f2e4be7be2770879c1c96a78
Dawid Sula <dsl@touk.pl>
2022-09-19 15:16:07 +0200

    Added CommonFlinkStreamingDeploymentManagerProvider

--- flink114/manager/src/main/scala/pl/touk/nussknacker/engine/management/CustomFlinkStreamingDeploymentManagerProvider.scala
+++ flink114/manager/src/main/scala/pl/touk/nussknacker/engine/management/CustomFlinkStreamingDeploymentManagerProvider.scala
@@ -1,26 +1,7 @@
 package pl.touk.nussknacker.engine.management
 
-import akka.actor.ActorSystem
-import com.typesafe.config.Config
-import pl.touk.nussknacker.engine.BaseModelData
-import pl.touk.nussknacker.engine.api.deployment.{DeploymentManager, ProcessingTypeDeploymentService}
-import pl.touk.nussknacker.engine.management.common.CommonFlinkStreamingRestManager
-import sttp.client.{NothingT, SttpBackend}
+import pl.touk.nussknacker.engine.management.common.CommonFlinkStreamingDeploymentManagerProvider
 
-import scala.concurrent.{ExecutionContext, Future}
-
-class CustomFlinkStreamingDeploymentManagerProvider extends FlinkStreamingDeploymentManagerProvider {
-
-  import net.ceedubs.ficus.readers.ArbitraryTypeReader._
-  import net.ceedubs.ficus.Ficus._
-  import pl.touk.nussknacker.engine.util.config.ConfigEnrichments._
-
-  override def createDeploymentManager(modelData: BaseModelData, config: Config)
-                                      (implicit ec: ExecutionContext, actorSystem: ActorSystem,
-                                       sttpBackend: SttpBackend[Future, Nothing, NothingT], deploymentService: ProcessingTypeDeploymentService): DeploymentManager = {
-    val flinkConfig = config.rootAs[FlinkConfig]
-    new CommonFlinkStreamingRestManager(flinkConfig, modelData)
-  }
-
+class CustomFlinkStreamingDeploymentManagerProvider extends CommonFlinkStreamingDeploymentManagerProvider {
   override def name: String = "flink114Streaming"
 }
